632941c3e9d1c26817cb6658aa44b262d0f9267e
Richard Packer <richard.packer@le.ac.uk>
2023-02-09 14:26:47 +0000

    edit to extract snps

--- R/extract_SNPs.R
+++ R/extract_SNPs.R
@@ -206,10 +206,12 @@ snp_extractor <- function(genetic_file_guide,
 
   lapply(pvar_file_loc,recoding_variants)
 
+  # list all pgen files
+  plink_merge_list <- list.files(paste0(analysis_folder,"/temp_plink"),pattern = "_temp.pgen")
 
   if(length(unique(chr_to_search)==1)) {
 
-    plink_name <- stringr::str_remove(unique(chr_to_search),".pgen")
+    plink_name <- stringr::str_remove(unlist(plink_merge_list),".pgen")
     plink_location <- paste0(analysis_folder,"/temp_plink/",plink_name)
     plink_output_location <- paste0(analysis_folder,"/",variant_save_name)
     system(paste0(plink_exe," --make-pgen --pfile ",plink_location," --out ",plink_output_location))
